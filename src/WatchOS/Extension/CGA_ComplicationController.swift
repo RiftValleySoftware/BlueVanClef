6f45a55e5b263ec48eda46f3b362a3186a430ad5
Chris Marshall <chris@riftvalleysoftware.com>
2020-08-19 11:19:49 -0400

    I've added support for complications. Looks like I'll need to mess around with them, to make sure the right ones get displayed in the right places (copy/pasta issue). I tweaked the bundle IDs and signing to be Rift Valley Software.

--- src/WatchOS/Extension/CGA_ComplicationController.swift
+++ src/WatchOS/Extension/CGA_ComplicationController.swift
@@ -33,6 +33,191 @@ class CGA_ComplicationController: NSObject {
 }
 
 /* ###################################################################################################################################### */
+// MARK: - Private Methods
+/* ###################################################################################################################################### */
+extension CGA_ComplicationController {
+    /* ################################################################## */
+    /**
+     This is a generic template generator.
+     
+     - parameter for: The complication we're generating this for.
+     
+     - returns: a Complication Template object.
+     */
+    private func _makeTemplateObject(for inComplication: CLKComplication) -> CLKComplicationTemplate? {
+        #if DEBUG
+            print("Template requested for complication (Part 1): \(inComplication.family)")
+        #endif
+        
+        switch inComplication.family {
+        case .circularSmall:
+            if let templateImage = UIImage(named: "Complication/Circular") {
+                let templateTmp = CLKComplicationTemplateCircularSmallSimpleImage()
+                templateTmp.imageProvider = CLKImageProvider(onePieceImage: templateImage)
+                return templateTmp
+            }
+        case .extraLarge:
+            if let templateImage = UIImage(named: "Complication/Extra Large") {
+                let templateTmp = CLKComplicationTemplateExtraLargeStackImage()
+                templateTmp.line1ImageProvider = CLKImageProvider(onePieceImage: templateImage)
+                templateTmp.line2TextProvider = CLKRelativeDateTextProvider(date: Date(), style: CLKRelativeDateStyle.natural, units: [.day])
+                return templateTmp
+            }
+        default:
+            return _makeModularTemplateObject(for: inComplication)
+        }
+        
+        return nil
+    }
+    
+    /* ################################################################## */
+    /**
+     This is a modular template generator.
+     
+     - parameter for: The complication we're generating this for.
+     
+     - returns: a Complication Template object.
+     */
+    private func _makeModularTemplateObject(for inComplication: CLKComplication) -> CLKComplicationTemplate? {
+        #if DEBUG
+            print("Template requested for complication (Part 2): \(String(describing: inComplication.family))")
+        #endif
+        
+        switch inComplication.family {
+        case .modularSmall:
+            #if DEBUG
+                print("Template requested for modularSmall")
+            #endif
+            if let templateImage = UIImage(named: "Complication/Modular") {
+                let templateTmp = CLKComplicationTemplateModularSmallSimpleImage()
+                templateTmp.imageProvider = CLKImageProvider(onePieceImage: templateImage)
+                return templateTmp
+            }
+        case .modularLarge:
+            #if DEBUG
+                print("Template requested for modularLarge")
+            #endif
+            if let templateImage = UIImage(named: "Complication/Modular") {
+                let templateTmp = CLKComplicationTemplateModularLargeStandardBody()
+                templateTmp.headerImageProvider = CLKImageProvider(onePieceImage: templateImage)
+                templateTmp.headerTextProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                templateTmp.body1TextProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                templateTmp.body2TextProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                return templateTmp
+            }
+        default:
+            return _makeUtilitarianTemplateObject(for: inComplication)
+        }
+        
+        return nil
+    }
+    
+    /* ################################################################## */
+    /**
+     This is a utilitarian template generator.
+     
+     - parameter for: The complication we're generating this for.
+     
+     - returns: a Complication Template object.
+     */
+    private func _makeUtilitarianTemplateObject(for inComplication: CLKComplication) -> CLKComplicationTemplate? {
+        #if DEBUG
+            print("Template requested for complication (Part 3): \(String(describing: inComplication.family))")
+        #endif
+        
+        switch inComplication.family {
+        case .utilitarianSmall:
+            #if DEBUG
+                print("Template requested for utilitarianSmall")
+            #endif
+            if let templateImage = UIImage(named: "Complication/Utilitarian") {
+                let templateTmp = CLKComplicationTemplateUtilitarianSmallFlat()
+                templateTmp.imageProvider = CLKImageProvider(onePieceImage: templateImage)
+                templateTmp.textProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                return templateTmp
+            }
+        case .utilitarianLarge:
+            #if DEBUG
+                print("Template requested for utilitarianLarge")
+            #endif
+            if let templateImage = UIImage(named: "Complication/Utilitarian") {
+                let templateTmp = CLKComplicationTemplateUtilitarianLargeFlat()
+                templateTmp.imageProvider = CLKImageProvider(onePieceImage: templateImage)
+                templateTmp.textProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                return templateTmp
+            }
+        default:
+            return _makeGraphicTemplateObject(for: inComplication)
+        }
+        
+        return nil
+    }
+    
+    /* ################################################################## */
+    /**
+     This is a graphic template generator.
+     
+     - parameter for: The complication we're generating this for.
+     
+     - returns: a Complication Template object.
+     */
+    private func _makeGraphicTemplateObject(for inComplication: CLKComplication) -> CLKComplicationTemplate? {
+        #if DEBUG
+            print("Template requested for complication (Part 4): \(String(describing: inComplication.family))")
+        #endif
+        
+        switch inComplication.family {
+        case .graphicCircular:
+            #if DEBUG
+                print("Template requested for graphicCircular")
+            #endif
+            if  let image = UIImage(named: "Complication/Graphic Circular") {
+                let templateTmp = CLKComplicationTemplateGraphicCircularImage()
+                templateTmp.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+                return templateTmp
+            }
+        case .graphicCorner:
+            #if DEBUG
+                print("Template requested for graphicCorner")
+            #endif
+            if  let image = UIImage(named: "Complication/Graphic Corner") {
+                let templateTmp = CLKComplicationTemplateGraphicCornerCircularImage()
+                templateTmp.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+                return templateTmp
+            }
+        case .graphicBezel:
+            #if DEBUG
+                print("Template requested for graphicBezel")
+            #endif
+            if let image = UIImage(named: "Complication/Graphic Bezel") {
+                let templateTmp = CLKComplicationTemplateGraphicBezelCircularText()
+                let circularItem = CLKComplicationTemplateGraphicCircularImage()
+                circularItem.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+                templateTmp.circularTemplate = circularItem
+                templateTmp.textProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                return templateTmp
+            }
+        case .graphicRectangular:
+            #if DEBUG
+                print("Template requested for graphicRectangular")
+            #endif
+            if let templateImage = UIImage(named: "Complication/Graphic Corner") {
+                let templateTmp = CLKComplicationTemplateGraphicRectangularStandardBody()
+                templateTmp.headerImageProvider = CLKFullColorImageProvider(fullColorImage: templateImage)
+                templateTmp.headerTextProvider = CLKSimpleTextProvider(text: Bundle.main.appDisplayName)
+                templateTmp.body1TextProvider = CLKSimpleTextProvider(text: "")
+                templateTmp.body2TextProvider = CLKSimpleTextProvider(text: "")
+                return templateTmp
+            }
+        default:
+            break
+        }
+        
+        return nil
+    }
+}
+
+/* ###################################################################################################################################### */
 // MARK: - CLKComplicationDataSource Conformance -
 /* ###################################################################################################################################### */
 /**
@@ -41,13 +226,113 @@ class CGA_ComplicationController: NSObject {
 extension CGA_ComplicationController: CLKComplicationDataSource {
     /* ################################################################## */
     /**
+     This sets the current timeline entry for the complication.
+     
+     - parameter for: The complication we're generating this for.
+     - parameter withHandler: The inHandler method to be called.
      */
-    func getSupportedTimeTravelDirections(for inComplication: CLKComplication, withHandler inHandler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
+    func getCurrentTimelineEntry(for inComplication: CLKComplication, withHandler inHandler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
+        #if DEBUG
+            print("Timeline Entry Requested for: \(inComplication.family)")
+        #endif
+        if let templateObject = _makeTemplateObject(for: inComplication) {
+            switch inComplication.family {
+            case .modularLarge:
+                if  let tObject = templateObject as? CLKComplicationTemplateModularLargeStandardBody {
+                    inHandler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: tObject))
+                } else {
+                    inHandler(nil)
+                }
+            case .utilitarianSmall:
+                if  let tObject = templateObject as? CLKComplicationTemplateUtilitarianSmallFlat {
+                    inHandler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: tObject))
+                } else {
+                    inHandler(nil)
+                }
+            case .utilitarianLarge:
+                if  let tObject = templateObject as? CLKComplicationTemplateUtilitarianLargeFlat {
+                    inHandler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: tObject))
+                } else {
+                    inHandler(nil)
+                }
+            case .graphicRectangular:
+                if  let tObject = templateObject as? CLKComplicationTemplateGraphicRectangularStandardBody {
+                    inHandler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: tObject))
+                } else {
+                    inHandler(nil)
+                }
+            default:
+                inHandler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: templateObject))
+            }
+        } else {
+            inHandler(nil)
+        }
     }
     
     /* ################################################################## */
     /**
+     This sets the supported placeholder.
+     
+     - parameter complication: The complication we're generating this for.
+     - parameter withHandler: The inHandler method to be called.
      */
-    func getCurrentTimelineEntry(for inComplication: CLKComplication, withHandler inHandler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
+    public func getPlaceholderTemplateForComplication(complication inComplication: CLKComplication, withHandler inHandler: (CLKComplicationTemplate?) -> Void) {
+        #if DEBUG
+            print("Placeholder Requested for: \(inComplication.family)")
+        #endif
+        if let templateObject = _makeTemplateObject(for: inComplication) {
+            switch inComplication.family {
+            case .modularLarge:
+                if  let tObject = templateObject as? CLKComplicationTemplateModularLargeStandardBody {
+                    inHandler(tObject)
+                } else {
+                    inHandler(nil)
+                }
+            case .utilitarianSmall:
+                if  let tObject = templateObject as? CLKComplicationTemplateUtilitarianSmallFlat {
+                    inHandler(tObject)
+                } else {
+                    inHandler(nil)
+                }
+            case .utilitarianLarge:
+                if  let tObject = templateObject as? CLKComplicationTemplateUtilitarianLargeFlat {
+                    inHandler(tObject)
+                } else {
+                    inHandler(nil)
+                }
+            case .graphicRectangular:
+                if  let tObject = templateObject as? CLKComplicationTemplateGraphicRectangularLargeImage {
+                    inHandler(tObject)
+                } else {
+                    inHandler(nil)
+                }
+            default:
+                inHandler(templateObject)
+            }
+        } else {
+            inHandler(nil)
+        }
+    }
+    
+    /* ################################################################## */
+    /**
+     This sets the supported Time Travel directions (We don't do any).
+     
+     - parameter for: The complication we're generating this for.
+     - parameter withHandler: The inHandler method to be called.
+     */
+    func getSupportedTimeTravelDirections(for inComplication: CLKComplication, withHandler inHandler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
+        inHandler([])
+    }
+    
+    /* ################################################################## */
+    /**
+     This sets the template object for the complication.
+     
+     - parameter for: The complication we're generating this for.
+     - parameter withHandler: The inHandler method to be called.
+     */
+    func getLocalizableSampleTemplate(for inComplication: CLKComplication, withHandler inHandler: @escaping (CLKComplicationTemplate?) -> Void) {
+        inHandler(_makeTemplateObject(for: inComplication))
     }
 }
