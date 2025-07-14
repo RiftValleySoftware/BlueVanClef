043a9bfa0b7d56873df3c301e595ec120aced85f
Chris Marshall <chris@riftvalleysoftware.com>
2025-07-14 15:40:37 -0400

    Starting to assemble a new SwiftUI target for the app.

---
+++ Sources/BVCApp/BVC_App.swift
@@ -0,0 +1,18 @@
+//
+//  BVCAppApp.swift
+//  BVCApp
+//
+//  Created by Chris Marshall on 7/14/25.
+//  Copyright © 2025 The Great Rift Valley Software Company. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BVC_App: App {
+    var body: some Scene {
+        WindowGroup {
+            BVC_MainWrapper()
+        }
+    }
+}
---
+++ Sources/BVCApp/BVC_MainWrapper.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BVCApp
+//
+//  Created by Chris Marshall on 7/14/25.
+//  Copyright © 2025 The Great Rift Valley Software Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct BVC_MainWrapper: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
